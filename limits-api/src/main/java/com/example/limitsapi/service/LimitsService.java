dd52e6d887f62c6ac2c60f652e30a372110a9b64
marcus-bornman <marcus.bornman@gmail.com>
2023-07-26 09:58:10 +0200

    Altered handling of optional in LimitsService.java for readability while demoing

--- limits-api/src/main/java/com/example/limitsapi/service/LimitsService.java
+++ limits-api/src/main/java/com/example/limitsapi/service/LimitsService.java
@@ -5,14 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.openapitools.model.AccountLimitsResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+
 @Service
 @RequiredArgsConstructor
 public class LimitsService {
     private final AccountLimitsRepository accountLimitsRepository;
 
     public AccountLimitsResponse getAccountLimits(String accountNumber) {
-        var accountLimitsEntity = accountLimitsRepository.findByAccountNumber(accountNumber).orElseThrow();
+        var optionalAccountLimitsEntity = accountLimitsRepository.findByAccountNumber(accountNumber);
 
+        if (optionalAccountLimitsEntity.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        var accountLimitsEntity = optionalAccountLimitsEntity.get();
         var accountLimitsResponse = new AccountLimitsResponse();
         accountLimitsResponse.setMin(accountLimitsEntity.getMin());
         accountLimitsResponse.setMax(accountLimitsEntity.getMax());
